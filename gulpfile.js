91d84430ce234d1a70d5387a017704f099ea5fe8
Sanjay <arunsanjay.dev@gmail.com>
2018-03-01 18:28:22 +0530

    completed large hero block and intro content block

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,2 @@
-var gulp = require('gulp'), 
-watch = require('gulp-watch');
-
-gulp.task('default', function(){
-    console.log("horray -  you created a Gulp task");
-});
-
-gulp.task('html', function(){
-    console.log("Imagine something useful being done to your HTML here");
-});
-
-gulp.task('watch', function(){
-    watch('./app/index.html', function(){
-        console.log("Imagine something useful being done to your HTML here");
-    });
-});
+require('./app/gulp/tasks/style');
+require('./app/gulp/tasks/watch');
